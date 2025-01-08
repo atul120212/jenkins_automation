1dcbb3ff5295c7d6a56bcb4655f5aebaa2cbac20
Atul Sharma <87241497+atul120212@users.noreply.github.com>
2025-01-08 11:11:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/info")
 def lw():
-    return "Welcome to LW....no oooooooooo bye...."
+    return "Welcome to Atul's Code at Linux World.. "
 
 
 app.run(host='0.0.0.0')
